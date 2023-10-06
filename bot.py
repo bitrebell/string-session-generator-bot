a29a6f600c850923c557239a7a78a6bde6e79a29
AADIL SHIEKH <114225192+bitrebell@users.noreply.github.com>
2023-10-06 14:28:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ logging.getLogger().setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from pyrogram import Client
+from pyromod import listen
+
 
 API_ID = 10738943
 API_HASH = "da61e3a08b5ac78ce28b4a4cd854aeec"
